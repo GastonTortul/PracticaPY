dd9528ac03ed14d41d5944bb62bceb70d108095b
GastonTortul <tortulgasto7@gmail.com>
2024-04-28 21:56:38 -0300

    Update

--- Practica1.py
+++ Practica1.py
@@ -1,16 +1,14 @@
-numero = 1
+numero = 0
 lista = []
 
-while numero <= 100 :
+while numero < 101 :
     lista.append(numero)
-
-    if (lista[numero]%2) != 0 and (lista[numero]%3) == 0:
+    if (lista[numero]%2) != 0 and (lista[numero]%5) == 0:
+        lista.append(numero)
+    elif (lista[numero]%2) != 0 and (lista[numero]%3) == 0:
         numero = numero * 2
         print (numero)
-    elif (lista[numero]%2) != 0 and (lista[numero]%5) == 0:
-        print
     else:
         print(numero)
-
+        
     numero = numero + 1
-print[lista]
